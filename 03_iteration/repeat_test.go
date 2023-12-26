2d44ea6b5e79c583248683d81fe788f16a6fa438
Gabriel M <gmajivu@worldremit.com>
2023-12-26 23:13:13 +0300

    repeat: caller to specify repeat count

--- 03_iteration/repeat_test.go
+++ 03_iteration/repeat_test.go
@@ -1,10 +1,13 @@
 package repeat
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	t.Run("repeat string characters 5 times", func(t *testing.T) {
-		repeated := Repeat("a")
+	t.Run("repeat string characters n times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
 		expected := "aaaaa"
 
 		if repeated != expected {
@@ -15,6 +18,12 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 10)
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 4)
+	fmt.Println(repeated)
+	// Output: aaaa
 }
