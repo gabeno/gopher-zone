612264bd467a3e584da604f4c02ae1ef5cbe663b
Gabriel M <gmajivu@worldremit.com>
2023-12-27 18:45:48 +0300

    shapes: refactor tests using shape interface

--- 05_structs_methods_interfaces/shapes_test.go
+++ 05_structs_methods_interfaces/shapes_test.go
@@ -3,45 +3,44 @@ package shapes
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		got := rectangle.Perimeter()
-		want := 40.0
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	})
-
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{10.0}
-		got := circle.Perimeter()
-		want := 62.83185307179586
+	checkPerimeter := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Perimeter()
 
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
+	}
+
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		checkPerimeter(t, rectangle, 40.0)
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		checkPerimeter(t, circle, 62.83185307179586)
 	})
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		got := rectangle.Area()
-		want := 100.0
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	})
-
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{10.0}
-		got := circle.Area()
-		want := 314.1592653589793
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
+	}
+
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		checkArea(t, rectangle, 100.0)
+
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
