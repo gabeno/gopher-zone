6acf5a65b3b4b44b8d5d31e5d9b5c9a2d46dac2a
Gabriel M <gmajivu@worldremit.com>
2023-12-27 18:14:05 +0300

    shapes: broken
    ```
    ➜ gopher_zone/05_structs_methods_interfaces main ✗ (*) go test -cover -v
    ./shapes_test.go:18:20: cannot use circle (variable of type Circle) as Rectangle value in argument to Perimeter
    ./shapes_test.go:40:15: cannot use circle (variable of type Circle) as Rectangle value in argument to Area
    FAIL	gopher_zone/05_structs_methods_interfaces [build failed]
    ```

--- 05_structs_methods_interfaces/shapes_test.go
+++ 05_structs_methods_interfaces/shapes_test.go
@@ -3,21 +3,45 @@ package shapes
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
-	want := 40.0
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := Perimeter(rectangle)
+		want := 40.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := Perimeter(circle)
+		want := 31.42
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Area(rectangle)
-	want := 100.0
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := Area(rectangle)
+		want := 100.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := Area(circle)
+		want := 314.2
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 }
