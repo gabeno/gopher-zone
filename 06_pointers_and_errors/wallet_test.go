1805dc75553ef5abe3dcb73167a05d46227dc85b
Gabriel M <gmajivu@worldremit.com>
2023-12-28 22:09:37 +0300

    wallet: move helpers below tests for clarity

--- 06_pointers_and_errors/wallet_test.go
+++ 06_pointers_and_errors/wallet_test.go
@@ -5,24 +5,6 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
-		t.Helper()
-		got := wallet.Balance()
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	}
-
-	assertError := func(t testing.TB, got error, want error) {
-		t.Helper()
-		if got == nil {
-			t.Fatal("did not raise an error")
-		}
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
-	}
-
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(0.0)}
 		wallet.Deposit(Bitcoin(10.0))
@@ -45,4 +27,22 @@ func TestWallet(t *testing.T) {
 		assertError(t, err, ErrInsufficientFunds)
 		assertBalance(t, wallet, startingBalance)
 	})
+}
+
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertError(t testing.TB, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("did not raise an error")
+	}
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
