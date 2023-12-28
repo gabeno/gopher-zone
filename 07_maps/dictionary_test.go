cbc1a5b858f0863905e9ee1ae452e670709f70d1
Gabriel M <gmajivu@worldremit.com>
2023-12-28 23:35:41 +0300

    dictionary: add

---
+++ 07_maps/dictionary_test.go
@@ -0,0 +1,18 @@
+package dictionary
+
+import "testing"
+
+func TestDictionary(t *testing.T) {
+	dictionary := map[string]string{"test": "this is just a test"}
+
+	got := Search(dictionary, "test")
+	want := "this is just a test"
+
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	if got != want {
+		t.Errorf("got %q want %q given %q", got, want, "test")
+	}
+}
