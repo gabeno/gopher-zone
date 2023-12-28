9f942258cee4d4d0885143da98ed136398c578c8
Gabriel M <gmajivu@worldremit.com>
2023-12-28 14:12:08 +0300

    wallet: enable withdrawal

--- 06_pointers_and_errors/wallet_test.go
+++ 06_pointers_and_errors/wallet_test.go
@@ -6,15 +6,29 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10.0))
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(0.0)}
+		wallet.Deposit(Bitcoin(10.0))
 
-	got := wallet.Balance()
-	want := Bitcoin(10.0)
+		got := wallet.Balance()
+		want := Bitcoin(10.0)
 
-	fmt.Printf("address of balance in test is %p \n", &wallet.balance)
+		fmt.Printf("address of balance in test is %p \n", &wallet.balance)
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20.0)}
+		wallet.Withdraw(Bitcoin(5.0))
+
+		got := wallet.Balance()
+		want := Bitcoin(15.0)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
