00aad6cdcef9181a385479f853342290081aeee4
Gabriel M <gmajivu@worldremit.com>
2023-12-29 00:03:00 +0300

    dictionary: add err variable

--- 07_maps/dictionary.go
+++ 07_maps/dictionary.go
@@ -1,7 +1,15 @@
 package dictionary
 
+import "errors"
+
+var ErrWordNotFound = errors.New("word does not exist")
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrWordNotFound
+	}
+	return definition, nil
 }
--- 07_maps/dictionary_test.go
+++ 07_maps/dictionary_test.go
@@ -5,14 +5,28 @@ import "testing"
 func TestDictionary(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("existing word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("non existing word", func(t *testing.T) {
+		_, err := dictionary.Search("unkown")
+
+		assertError(t, err, ErrWordNotFound)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
 	if got != want {
 		t.Errorf("got %q want %q given %q", got, want, "test")
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
 	}
 }
