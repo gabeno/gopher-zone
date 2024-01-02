d27c0f57a9931382d3a4308bf0011e64b6bc631b
Gabriel M <gmajivu@worldremit.com>
2024-01-02 21:01:53 +0300

    dictionary: error out when a word already exists

--- 07_maps/dictionary.go
+++ 07_maps/dictionary.go
@@ -2,7 +2,10 @@ package dictionary
 
 import "errors"
 
-var ErrWordNotFound = errors.New("word does not exist")
+var (
+	ErrWordNotFound = errors.New("word does not exist")
+	ErrWordExists   = errors.New("word already exists")
+)
 
 type Dictionary map[string]string
 
@@ -14,6 +17,15 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrWordNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
 }
