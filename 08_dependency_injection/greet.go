ff9ecba9a5318f2137029c093d3a08ea58e3bb61
Gabriel M <gmajivu@worldremit.com>
2024-01-02 21:58:55 +0300

    di: can not use os.stdout

--- 08_dependency_injection/greet.go
+++ 08_dependency_injection/greet.go
@@ -3,8 +3,13 @@ package greet
 import (
 	"bytes"
 	"fmt"
+	"os"
 )
 
 func Greet(writer *bytes.Buffer, name string) {
 	fmt.Fprintf(writer, "Hello %s", name)
+}
+
+func main() {
+	Greet(os.Stdout, "Hello Gabe")
 }
