b2c681eed13edc9e5f4553e39691c9b87d2b8b51
Gabriel M <gmajivu@worldremit.com>
2024-01-03 22:32:19 +0300

    crawler: broken concurrency
    ```
    ➜ gopher_zone/10_concurrency main ✓ go test -bench=.
    --- FAIL: TestCheckWebsites (0.00s)
        websites_checker_test.go:29: got map[waat://furhurterwe.geds:false] want map[http://blog.gypsydave5.com:true http://google.com:true waat://furhurterwe.geds:false]
    FAIL
    exit status 1
    FAIL	gopher_zone/10_concurrency	1.330s
    ➜ gopher_zone/10_concurrency main ✗ (*) go test -cover -v
    === RUN   TestCheckWebsites
        websites_checker_test.go:29: got map[] want map[http://blog.gypsydave5.com:true http://google.com:true waat://furhurterwe.geds:false]
    --- FAIL: TestCheckWebsites (0.00s)
    FAIL
    coverage: 100.0% of statements
    exit status 1
    FAIL	gopher_zone/10_concurrency	0.956s
    ```
    
    - we use goroutines invoked via anonymous function
    - the goroutines have access to the lexical scope in which they are
      declared in.

--- 10_concurrency/website_schecker.go
+++ 10_concurrency/website_schecker.go
@@ -5,7 +5,9 @@ type WebsitesChecker func(string) bool
 func CheckWebsites(wc WebsitesChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func() {
+			results[url] = wc(url)
+		}()
 	}
 	return results
 }
