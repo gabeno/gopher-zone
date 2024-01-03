9e7b2d35bd46fe31c90c87ed3ea7786f887f9acd
Gabriel M <gmajivu@worldremit.com>
2024-01-03 22:37:23 +0300

    crawler: pass in url for each goroutine
    ```
    ➜ gopher_zone/10_concurrency main ✓ go test -cover -v
    === RUN   TestCheckWebsites
        websites_checker_test.go:29: got map[http://google.com:true] want map[http://blog.gypsydave5.com:true http://google.com:true waat://furhurterwe.geds:false]
    --- FAIL: TestCheckWebsites (0.00s)
    FAIL
    coverage: 100.0% of statements
    exit status 1
    FAIL	gopher_zone/10_concurrency	0.860s
    ```
    
    - test still broken. we are only getting two results

--- 10_concurrency/website_schecker.go
+++ 10_concurrency/website_schecker.go
@@ -5,9 +5,9 @@ type WebsitesChecker func(string) bool
 func CheckWebsites(wc WebsitesChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 	for _, url := range urls {
-		go func() {
-			results[url] = wc(url)
-		}()
+		go func(u string) {
+			results[u] = wc(u)
+		}(url)
 	}
 	return results
 }
