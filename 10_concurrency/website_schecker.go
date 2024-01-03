ccc4170a44f080222c20e6dd686174a0336f62b4
Gabriel M <gmajivu@worldremit.com>
2024-01-03 22:53:19 +0300

    crawler: use channles
    - we do not have race condition any more
    
    ```
    ➜ gopher_zone/10_concurrency main ✗ (*) go test -race
    PASS
    ok  	gopher_zone/10_concurrency	3.057s
    ➜ gopher_zone/10_concurrency main ✗ (*) go test -cover -v
    === RUN   TestCheckWebsites
    --- PASS: TestCheckWebsites (0.00s)
    PASS
    coverage: 100.0% of statements
    ok  	gopher_zone/10_concurrency	1.469s
    ➜ gopher_zone/10_concurrency main ✗ (*) go test -bench=.
    goos: darwin
    goarch: arm64
    pkg: gopher_zone/10_concurrency
    BenchmarkCheckWebsites-12    	      55	  21195860 ns/op
    PASS
    ok  	gopher_zone/10_concurrency	2.667s
    ```
    
    we use channel for communication between parent process and each
    goroutine that it makes to do the work of checking a website url.
    - channel send statement: `resultChannel <- result{u, wc(u)}`
    - channel receive statement: `r := <-resultChannel`
    - sending results into a channel allows us to control the timing of each
      write into the results map, ensuring it happens only once at a time.
    - Although each of the calls of wc, and each send to the result channel,
      is happening in parallel inside its own process, each of the results is
      being dealt with one at a time as we take values out of the result
      channel with the receive expression.

--- 10_concurrency/website_schecker.go
+++ 10_concurrency/website_schecker.go
@@ -1,16 +1,25 @@
 package crawler
 
-import "time"
-
 type WebsitesChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsitesChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
+
 	for _, url := range urls {
 		go func(u string) {
-			results[u] = wc(u)
+			resultChannel <- result{u, wc(u)}
 		}(url)
 	}
-	time.Sleep(2 * time.Second)
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
+
 	return results
 }
