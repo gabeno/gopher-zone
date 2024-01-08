3587d18ffafa7239be04ae0c6db74c5031691562
Gabriel M <gmajivu@worldremit.com>
2024-01-08 12:27:50 +0300

    racer: create test servers which we can control and avoid flakiness

--- 11_select/racer_test.go
+++ 11_select/racer_test.go
@@ -1,10 +1,22 @@
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowUrl := "http://www.facebook.com"
-	fastUrl := "http://www.quii.dev"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	slowUrl := slowServer.URL
+	fastUrl := fastServer.URL
 
 	want := fastUrl
 	got := Racer(slowUrl, fastUrl)
@@ -12,4 +24,7 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
 }
