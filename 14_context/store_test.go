3395e3724b92ea8c49ead061e57f620c7a47982b
Gabriel M <gmajivu@worldremit.com>
2024-01-10 22:25:44 +0300

    store: cancel request
    - we derive a new context `cancellingCtx` from our request which gives
      us a `cancel` function.
    - we schedule to call the `cancel` function after 5ms using `timeAfter`
      function.
    - we use this new context in our request by calling `request.WithContext`

--- 14_context/store_test.go
+++ 14_context/store_test.go
@@ -1,29 +1,60 @@
 package store
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
-type StubStore struct {
-	response string
+type SpyStore struct {
+	response  string
+	cancelled bool
 }
 
-func (s *StubStore) Fetch() string {
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
 	return s.response
 }
 
+func (s *SpyStore) Cancel() bool {
+	s.cancelled = true
+	return s.cancelled
+}
+
 func TestServer(t *testing.T) {
-	data := "Hello, world"
-	svr := Server(&StubStore{data})
+	t.Run("happy path", func(t *testing.T) {
+		data := "Hello, world"
+		svr := Server(&SpyStore{response: data, cancelled: false})
 
-	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
 
-	svr.ServeHTTP(response, request)
+		svr.ServeHTTP(response, request)
 
-	if response.Body.String() != data {
-		t.Errorf("got %s want %s", response.Body.String(), data)
-	}
+		if response.Body.String() != data {
+			t.Errorf("got %s want %s", response.Body.String(), data)
+		}
+	})
+
+	t.Run("tell store to cancel work if request is cancelled", func(t *testing.T) {
+		data := "hello, world"
+		store := &SpyStore{response: data, cancelled: false}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		if !store.cancelled {
+			t.Error("store was not told to cancel")
+		}
+	})
 }
