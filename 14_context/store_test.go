5edcc16671f24b8176556eb2c806001e04f277be
Gabriel M <gmajivu@worldremit.com>
2024-01-11 09:47:15 +0300

    store: pass context to store
    - idiomatically it does not make sense for server to cancel `Store`.
      what if `Store` happens to have other dependencies on other slow
      running processes? we will have to make sure that `store.Cancel`
      correctly propagates the cancellation to all its depencencies
    - instead pass through `context` to out `Store` and let it be
      responsible. that way it can also pass the context to its dependants
      and they too can be responsible for cancelling themselves.
    - we make our spy act like a real method that works with context.
    - we are simulating a slow process that builds a string slowly by
      appending the string character by character in the goroutine. when
      finished the goroutine writes to the string to the `data` channel. the
      goroutine listens to `ctx.Done()` and will stop the work if a signal
      is sent in that channel. we use another select to either wait for the
      goroutine to finish its work or cancellation to occur.
    - use a similar approach when writing functions and methods that use
      `context`.

--- 14_context/store_test.go
+++ 14_context/store_test.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -9,39 +10,40 @@ import (
 )
 
 type SpyStore struct {
-	response  string
-	cancelled bool
-	t         *testing.T
+	response string
+	t        *testing.T
 }
 
-func (s *SpyStore) Fetch() string {
-	time.Sleep(100 * time.Millisecond)
-	return s.response
-}
+func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
+	data := make(chan string, 1)
 
-func (s *SpyStore) Cancel() bool {
-	s.cancelled = true
-	return s.cancelled
-}
+	go func() {
+		var result string
+		for _, c := range s.response {
+			select {
+			case <-ctx.Done():
+				log.Println("spy got cancelled")
+				return
+			default:
+				time.Sleep(10 * time.Millisecond)
+				result += string(c)
+			}
+		}
+		data <- result
+	}()
 
-func (s *SpyStore) assertWasCancelled() {
-	s.t.Helper()
-	if !s.cancelled {
-		s.t.Error("store was not told to cancel")
-	}
-}
-
-func (s *SpyStore) assertWasNotCancelled() {
-	s.t.Helper()
-	if s.cancelled {
-		s.t.Error("it should not have cancelled the store")
+	select {
+	case <-ctx.Done():
+		return "", ctx.Err()
+	case res := <-data:
+		return res, nil
 	}
 }
 
 func TestServer(t *testing.T) {
 	t.Run("happy path", func(t *testing.T) {
 		data := "Hello, world"
-		store := &SpyStore{response: data, cancelled: false, t: t}
+		store := &SpyStore{response: data, t: t}
 		svr := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
@@ -52,13 +54,11 @@ func TestServer(t *testing.T) {
 		if response.Body.String() != data {
 			t.Errorf("got %s want %s", response.Body.String(), data)
 		}
-
-		store.assertWasNotCancelled()
 	})
 
 	t.Run("tell store to cancel work if request is cancelled", func(t *testing.T) {
 		data := "hello, world"
-		store := &SpyStore{response: data, cancelled: false, t: t}
+		store := &SpyStore{response: data, t: t}
 		svr := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
@@ -70,7 +70,5 @@ func TestServer(t *testing.T) {
 		response := httptest.NewRecorder()
 
 		svr.ServeHTTP(response, request)
-
-		store.assertWasCancelled()
 	})
 }
