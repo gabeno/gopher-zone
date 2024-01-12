4b9e9861e27a5c156e106a4bbed46ae4a9d4769c
Gabriel M <gmajivu@worldremit.com>
2024-01-12 10:05:21 +0300

    convert: add more logic + test cases

--- 15_property_testing/roman_numerals.go
+++ 15_property_testing/roman_numerals.go
@@ -7,6 +7,12 @@ func ConvertToRomans(n int) string {
 
 	for n > 0 {
 		switch {
+		case n > 9:
+			results.WriteString("X")
+			n -= 10
+		case n > 8:
+			results.WriteString("IX")
+			n -= 9
 		case n > 4:
 			results.WriteString("V")
 			n -= 5
--- 15_property_testing/roman_numerals_test.go
+++ 15_property_testing/roman_numerals_test.go
@@ -13,6 +13,10 @@ func TestRomanNumerals(t *testing.T) {
 		{Description: "3 converts to III", NumberToConvert: 3, Want: "III"},
 		{Description: "4 converts to IV", NumberToConvert: 4, Want: "IV"},
 		{Description: "5 converts to V", NumberToConvert: 5, Want: "V"},
+		{Description: "6 converts to VI", NumberToConvert: 6, Want: "VI"},
+		{Description: "9 converts to IX", NumberToConvert: 9, Want: "IX"},
+		{Description: "10 converts to X", NumberToConvert: 10, Want: "X"},
+		{Description: "12 converts to XII", NumberToConvert: 12, Want: "XII"},
 	}
 
 	for _, test := range cases {
