360e7d306bc48c0801562c26d2c9d15d7abd704b
Gabriel M <gmajivu@worldremit.com>
2024-01-12 16:45:23 +0300

    generics: assert on strings and indeed any other type by using interface{} type

--- 19_generics/generics_test.go
+++ 19_generics/generics_test.go
@@ -7,16 +7,21 @@ func TestAssertFunctions(t *testing.T) {
 		AssertEqual(t, 1, 1)
 		AssertNotEqual(t, 1, 2)
 	})
+
+	t.Run("assert on strings", func(t *testing.T) {
+		AssertEqual(t, "gabe", "gabe")
+		AssertNotEqual(t, "gabe", "gobe")
+	})
 }
 
-func AssertEqual(t testing.TB, got, want int) {
+func AssertEqual(t testing.TB, got, want interface{}) {
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %+v want %+v", got, want)
 	}
 }
 
-func AssertNotEqual(t testing.TB, got, want int) {
+func AssertNotEqual(t testing.TB, got, want interface{}) {
 	if got == want {
-		t.Errorf("did not want %d", got)
+		t.Errorf("did not want %+v", got)
 	}
 }
