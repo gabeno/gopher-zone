a6dba60ab2f639e311a4b556032566c418a58e47
Gabriel M <gmajivu@worldremit.com>
2024-01-13 22:18:15 +0300

    reduce function works on more examples

--- 04_arrays_and_slices/sum_test.go
+++ 04_arrays_and_slices/sum_test.go
@@ -34,3 +34,27 @@ func TestSumAllTails(t *testing.T) {
 		t.Errorf("got %v expected %v", got, expected)
 	}
 }
+
+func TestReduce(t *testing.T) {
+	t.Run("multiplication of all elements", func(t *testing.T) {
+		multiply := func(x, y int) int {
+			return x * y
+		}
+
+		AssertEqual(t, Reduce([]int{1, 2, 3}, multiply, 1), 6)
+	})
+
+	t.Run("concatenate strings", func(t *testing.T) {
+		concatenator := func(a, b string) string {
+			return a + b
+		}
+
+		AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenator, ""), "abc")
+	})
+}
+
+func AssertEqual[T comparable](t testing.TB, got, want T) {
+	if got != want {
+		t.Errorf("got %#v want %#v", got, want)
+	}
+}
