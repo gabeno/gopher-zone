27a1c68ef0c127ef6d6c3f5e5b49d44a1eb19ec8
Gabriel Majivu <gmajivu@gmail.com>
2025-02-17 11:57:07 +0300

    hello

---
+++ playground/hello-world.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// start
+	fmt.Println("hi there")
+
+	// concatenate strings
+	fmt.Println("go" + "lang")
+
+	// integers and floats
+	fmt.Println("1+1=", 1+1)
+	fmt.Println("7.0/3.0=", 7.0/3.0)
+
+	// boolean with boolean operators
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
