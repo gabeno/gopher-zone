86e39d6397e344facc82f9369a8d9cc8b3ac83e3
Gabriel Majivu <gmajivu@gmail.com>
2025-02-25 15:18:51 +0300

    variables: more examples

--- playground/variables/variables.go
+++ playground/variables/variables.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	// declare a variable
+	// declare a variable with `var` keyword
 	var a = "inital"
 	fmt.Println(a)
 
 	// declare multiple variables
-	var b, c int = 1, 2
-	fmt.Println(b, c)
+	var x, c int = 1, 2
+	fmt.Println(x, c)
 
 	// non-initialized variables are zero-valued
 	var d int
@@ -18,4 +18,17 @@ func main() {
 	// := syntax for declaring and initializing a variable
 	e := "apple"
 	fmt.Println(e)
+
+	// variable names can have unicode characters besides upper and lowercase and underscore
+	手紙 := "letter"
+	fmt.Println(手紙)
+
+	fmt.Println()
+
+	// variables without intial values are set to their corresponding zero values
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("initial values -> int:%v float:%v bool:%v string:%q\n", i, f, b, s)
 }
