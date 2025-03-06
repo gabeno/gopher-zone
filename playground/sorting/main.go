d68d341159a98ccb0eb35e941de1f817515b94f6
Gabe <gmajivu@gmail.com>
2025-03-06 23:30:30 +0300

    custom sort criteria

--- playground/sorting/main.go
+++ playground/sorting/main.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
 	"slices"
 )
@@ -19,4 +20,30 @@ func main() {
 
 	n_isSorted := slices.IsSorted(n)
 	fmt.Println("sorted n", n_isSorted)
+
+	// custom sort
+
+	// - by length of strings
+	fruits := []string{"peach", "banana", "kiwi"}
+	// custom sort function
+	lenCmp := func(a, b string) int {
+		return cmp.Compare(len(a), len(b))
+	}
+	slices.SortFunc(fruits, lenCmp)
+	fmt.Println(fruits)
+
+	// - by age of person
+	type Person struct {
+		name string
+		age  int
+	}
+	people := []Person{
+		{name: "Jay", age: 37},
+		{name: "Trish", age: 25},
+		{name: "Alex", age: 72},
+	}
+	slices.SortFunc(people, func(a, b Person) int {
+		return cmp.Compare(a.age, b.age)
+	})
+	fmt.Println(people)
 }
