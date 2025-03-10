88e9cc7bfa63694c31d73f664cf3fea06a8604e0
Gabe <gmajivu@gmail.com>
2025-03-10 23:07:27 +0300

    command line flags

---
+++ playground/command-line-flags/main.go
@@ -0,0 +1,27 @@
+// handle flags which are a way to specify options for cli programs
+
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	// declare flags
+	wordPtr := flag.String("word", "foo", "a string")
+	numbPtr := flag.Int("numb", 42, "an int")
+	forkPtr := flag.Bool("fork", false, "a bool")
+
+	// use var declared elsewhere in the program
+	var svar string
+	flag.StringVar(&svar, "svar", "bar", "a string var")
+
+	flag.Parse()
+
+	fmt.Println("word:", *wordPtr)
+	fmt.Println("numb:", *numbPtr)
+	fmt.Println("fork:", *forkPtr)
+	fmt.Println("svar:", svar)
+	fmt.Println("tail:", flag.Args())
+}
