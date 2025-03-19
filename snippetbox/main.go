17366fd07ae980371fdd87f00a938f81e222f2b7
Gabe <gmajivu@gmail.com>
2025-03-19 09:11:13 +0300

    add create snippet handler

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -27,11 +27,16 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("snippet create"))
 }
 
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("save a new snippet ..."))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
 	log.Print("starting server on :4000")
 
