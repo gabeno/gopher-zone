4fed7239b4703aa801fa7b60f9a07c28b577f3bc
Gabe <gmajivu@gmail.com>
2025-04-07 11:28:19 +0300

    serve static files

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -7,6 +7,12 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	// create a file server
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
