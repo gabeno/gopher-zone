074eb8c821a8a120ebeeb25d0180cd938c7050d2
Gabe <gmajivu@gmail.com>
2025-04-16 11:30:55 +0300

    centralize error handling

---
+++ snippetbox/cmd/web/helpers.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"runtime/debug"
+)
+
+// helper that writes a log entry at Error level
+// sends a generic 500 Internal Server Error response to the user
+func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
+	var (
+		method = r.Method
+		uri    = r.URL.RequestURI()
+		trace  = string(debug.Stack())
+	)
+
+	app.logger.Error(err.Error(), "method", method, "uri", uri, "trace", trace)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
